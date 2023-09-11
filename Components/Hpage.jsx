cc7e398c10f3f1d3c06b2d0336e27074c1b759f9
lee14k <kmkovach@mtu.edu>
2023-09-11 02:28:49 -0400

    home

--- Components/Hpage.jsx
+++ Components/Hpage.jsx
@@ -2,7 +2,7 @@
 function Hpage() {
     return (
   <div className="home">
-  <span className="dot">  <h2 className="text-8xl font-bold underline hhead">Steph Heise</h2>    
+  <span className="dot">  <h2 className="font-bold underline hhead">Steph Heise</h2>    
 </span>
 
 
